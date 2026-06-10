104aef5bad62fef4f48438de97650e8ba9250d81
fishtreesugar <fishtreesugar@gmail.com>
2026-06-10 12:12:02 -0500

    Align v0.1.0-rc.1 checksums

--- checksum.exs
+++ checksum.exs
@@ -1,2 +1,5 @@
 %{
+  "onnxruntime-nif-2.17-aarch64-apple-darwin-0.1.0-rc.1.tar.gz" => "sha256:631c5be4bf9cb5413dc7f6968b8d8cdece0f3df6b7c4abf2b56321620b013a48",
+  "onnxruntime-nif-2.17-aarch64-linux-gnu-0.1.0-rc.1.tar.gz" => "sha256:a2ae8a375514d60bbf46ace88248f861309d35437e79122d8d8c3c3cfcdd2205",
+  "onnxruntime-nif-2.17-x86_64-linux-gnu-0.1.0-rc.1.tar.gz" => "sha256:c5ea71dcc5343db981a376df3473ac1887054921db509d59dac09d76e007ed32",
 }
